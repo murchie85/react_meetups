366d7640d2d9aab22850291bb7298140ff798304
murchie85 <murchie85@gmail.com>
2022-04-26 23:51:29 +0100

    m

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 
 function App() {
-  return (
-    <div>Hello!</div>
-  );
+  return <div>
+      <h1>My Todos</h1>
+      
+      <div className="card">
+        <h2>Title</h2>
+        <div>
+            <button>Delete</button>
+        </div>
+      </div>
+
+    </div>;
+  ;
 }
 
 export default App;
